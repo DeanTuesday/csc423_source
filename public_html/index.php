cdda92e8b1728e8f71cfb25cdac873b50c5755b9
Will Dean <williamdean19@gmail.com>
2016-10-22 17:59:30 -0400

    made Update Vendor form, no php yet

--- public_html/index.php
+++ public_html/index.php
@@ -1,4 +1,5 @@
 <?php
-	echo 'Hello World';
-	echo 'My name is Will Dean';
+	echo 'Hello World<br>';
+	echo 'My name is Will Dean, this is my contribution:<br>';
+	echo '<a href="UpdateVendor.html">Update A Vendor</a><br>';
 ?>
