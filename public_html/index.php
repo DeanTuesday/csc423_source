1b36e7a0321d650c1c2e6494b4c75516db840e44
Corey <ceast4@u.brockport.edu>
2016-10-23 13:35:15 -0400

    intro to corey's files

--- public_html/index.php
+++ public_html/index.php
@@ -2,4 +2,7 @@
 	echo 'Hello World<br>';
 	echo 'My name is Will Dean, this is my contribution:<br>';
 	echo '<a href="UpdateVendor.html">Update A Vendor</a><br>';
+	echo 'My name is Corey Easton, this is my contribution:<br>';
+	echo '<a href="AddItem.html">Add Item</a><br>';
+	echo '<a href="UpdateItem.html">Update Item</a><br>';
 ?>
