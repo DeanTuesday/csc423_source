73a8cf91286ad2f52620626c616cac5a35eca54f
Will Dean <williamdean19@gmail.com>
2016-10-23 17:43:59 -0400

    some progress on php - WD

---
+++ public_html/handleUpdateInfo.php
@@ -0,0 +1,15 @@
+<?php
+		if(isset($_POST['SentCheck']))
+		{
+			$vendorName="Willbur";
+			echo "Willbur";
+		};
+/*
+		if(isset($_POST['']))
+		{
+			$connection= mysql_connect();
+			$db_found= mysql_select_db();
+			$selectVendorQuery= "select * from Vendors";
+		}
+*/
+?>
--- public_html/index.php
+++ public_html/index.php
@@ -1,5 +1,5 @@
 <?php
 	echo 'Hello World<br>';
 	echo 'My name is Will Dean, this is my contribution:<br>';
-	echo '<a href="UpdateVendor.html">Update A Vendor</a><br>';
+	echo '<a href="UpdateVendor.php">Update A Vendor</a><br>';
 ?>
