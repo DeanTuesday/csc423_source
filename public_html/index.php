625bc680dc323e0de97afcdfba4edb67f3c67e68
mason <mmasc1@u.brockport.edu>
2016-10-25 14:53:20 -0700

    update

--- public_html/index.php
+++ public_html/index.php
@@ -4,6 +4,8 @@
 	echo '<a href="SelectVendorToUpdate.php">Update A Vendor</a><br>';
 	echo 'My name is Corey Easton, this is my contribution:<br>';
 	echo '<a href="selectItemToUpdate.php">Update A Item</a><br>';
+	echo 'My name is Mason Mascle, this is my contribution:<br>';
+	echo '<a href="SelectCustomerToUpdate.php">Update A Customer</a><br>';
 	
 ?>
 
