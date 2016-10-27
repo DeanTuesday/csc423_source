53814c3b0489fab7f991c2dd043805f54fa3dafa
yahwei <mymei3636@gmail.com>
2016-10-26 22:58:03 -0400

    addVendor  localhost

--- public_html/addVendor2.php
+++ public_html/addVendor2.php
@@ -75,7 +75,7 @@
     <body>
 		<?php
 			if(isset($_POST['submitCheck'])) {
-				$addr = '127.0.0.2';
+				$addr = 'localhost';
 				$user = 'wdean2';
 				$pass = 'csc423?';
 				$db = 'fal16_csc423_wdean2';
