50ab2d4ad5c1b84855ceaab3398a278339ba982b
Corey <ceast4@u.brockport.edu>
2016-10-27 12:00:04 -0400

    test24

--- public_html/UpdateItem.php
+++ public_html/UpdateItem.php
@@ -141,14 +141,16 @@ echo "
 				<option value='26'>GIBSON ENTERPRISES INC</option>
 				<option value='27'>J M SMUCKER (COFFEE PREPAID)</option>
 				<option value='28'>CLEMENT PAPPAS & CO INC (P-MOUNT)</option>
-				
+				</td>
+				</tr>
 				<tr>
+				<td>
 				<center><br><input type='submit' value='Submit Changes' </center>		<center><br><input type='button' value='Undo Changes'></center>	
 							
 							<input name='SubmitChangesCheck' type='hidden' value='sent'>
 				</tr>
 			</td>
-</tr>
+
 			</table>
 			
 		</form>
