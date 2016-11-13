1a48c1baa148f390c589840bd7e476ba10f113e0
mason <mmasc1@u.brockport.edu>
2016-11-13 18:16:34 -0500

    mason initial commit

---
+++ public_html/dbScriptOrders.php
@@ -0,0 +1,150 @@
+<?php
+
+// Header file will use this to set the page title
+$PageTitle="Vendor Orders";
+
+// Header file will use this function to link your page to other css or js files
+function customPageHeader(){
+?>
+<!-- Add any CSS or JS files here -->
+    <link rel="stylesheet" href="./css/styles.css" />
+<?php }
+
+// Run the DB Script and output any errors for debugging
+if(isset($_POST['submit'])){
+    // Always connect to DB
+    $config = include('./inc/config.php');
+    $conn = new mysqli($config['addr'], $config['user'], $config['pass'], $config['db']);
+    if($conn->connect_errno){
+        echo "Error: Failed to make a MySQL connection, here is why: \n";
+        echo "Errno: " . $mysqli->connect_errno . "\n";
+        echo "Error: " . $mysqli->connect_error . "\n";
+        exit;
+    }
+
+    // Always grab customer info from the form
+    $orderId = $_POST['orderId'];
+    $vendorId = $_POST['VendorId'];
+    $storeId = $_POST['storeId'];
+    $datetimeOrder = $_POST['datetimeOrder'];
+    $status = $_POST['status'];
+    $datetimeFullfillment = $_POST['datetimeFullfillment'];
+    
+
+    // Only run the following query if we are inserting
+    if(isset($_POST['addOrder'])){
+        $query =    "insert into Customer (CustomerId, Name, Address, City, State, ZIP, Phone, Email) ".
+                    "values ('$cId', '$name', '$address', '$city', '$state', '$zip', '$phone', '$email')";
+   
+        $result = $conn->query($query);
+        if(!$result) {
+            echo "Error: Our query failed to execute and here is why: \n";
+            echo "Query: " . $query . "\n";
+            echo "Errno: " . $mysqli->errno . "\n";
+            echo "Error: " . $mysqli->error . "\n";
+            exit;
+        }
+        else {
+            echo "Customer created successfully" ;
+        }
+    }
+
+    // Only run the following query if we are updating
+    if(isset($_POST['updateOrder'])){
+        $query= "update Customer ".
+                "set Name='$name', Address='$address', City='$city',State='$state', ZIP='$zip', Phone='$phone', Email='$email' ".
+                "where CustomerId='$cId'";
+
+        $result = $conn->query($query);
+        if(!$result) {
+            echo "Error: Our query failed to execute and here is why: \n";
+            echo "Query: " . $query . "\n";
+            echo "Errno: " . $mysqli->errno . "\n";
+            echo "Error: " . $mysqli->error . "\n";
+            exit;
+        }
+        else {
+            echo "Customer updateded successfully" ;
+        }
+    }
+	// Only run the following query if we are viewing vendors orders
+    if(isset($_POST['viewOrders'])){
+        
+		$password = $_POST['Password'];
+		// check to see if vendor id and password are valid
+		$vquery= "SELECT * FROM Vendor WHERE VendorId='$vendorId' AND Password='$password'";
+		
+		$result = $conn->query($vquery);
+		
+		  if(!$result) {
+            echo "Error: Our query failed to execute and here is why: \n";
+            echo "Query: " . $query . "\n";
+            echo "Errno: " . $mysqli->errno . "\n";
+            echo "Error: " . $mysqli->error . "\n";
+            exit;
+        }
+		else{
+		// if valid proceed to get vendors orders
+		$query= "SELECT * FROM Orders WHERE VendorId='$vendorId'";
+
+        $result = $conn->query($query);
+        if(!$result) {
+            echo "Error: Our query failed to execute and here is why: \n";
+            echo "Query: " . $query . "\n";
+            echo "Errno: " . $mysqli->errno . "\n";
+            echo "Error: " . $mysqli->error . "\n";
+            exit;
+        }
+     
+    
+    // Always close the connection
+    
+
+else{
+      ?>
+    
+<table border="2">
+  <thead>
+    <tr>
+      <th>OrderId</th>
+      <th>VendorId</th>
+      <th>StoreId</th>
+      <th>DateTimeOfOrder</th>
+	  <th>Status</th>
+	  <th>DateTimeOfFullfilment</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php 
+      if( mysql_num_rows( $selectRes )==0 ){
+        echo '<tr><td colspan="4">No Rows Returned</td></tr>';
+      }else{
+        while( $row = mysql_fetch_assoc( $selectRes ) ){
+          echo "<tr><td>{$row['OrderId']}</td><td>{$row['VendorId']}</td><td>{$row['StoreId']}</td><td>{$row['DateTimeOfOrder']}</td><td>{$row['Status']}</td><td>{$row['DateTimeOfFullfilment']}</td></tr>\n";
+        }
+      }
+    
+  }
+  }
+}
+}
+$conn->close();
+}
+
+// Header
+include_once('./templates/header.php');
+?>
+
+<!-- Body Content Goes Here -->
+<table>
+    <tr>
+        <td><a href='./index.php' class='button'>Home</a></td>
+        <td><a href='./addCustomer.php' class='button'>Add A Customer</a></td>
+        <td><a href='./selectCustomerToUpdate.php' class='button'>Update A Customer</a></td>
+    </tr>
+</table>
+
+<?php
+// Footer
+include_once('./templates/footer.php');
+?>
--- public_html/viewOrderLogin.php
+++ public_html/viewOrderLogin.php
@@ -17,42 +17,15 @@ include_once('./templates/header.php');
 
 <!-- Body Content Goes Here -->
 <h1>Login<h1>
-<form action='#' method='post'>
+<form  action="dbScriptOrders.php" name="viewOrders" id="viewOrders" method='post'>
 <table cellspacing='5' align='center'>
-<tr><td>User name:</td><td><input type='text' name='name'/></td></tr>
-<tr><td>Password:</td><td><input type='password' name='pwd'/></td></tr>
+<tr><td>VendorId:</td><td><input type='text' name='VendorId'/></td></tr>
+<tr><td>Password:</td><td><input type='password' name='Password'/></td></tr>
 <tr><td></td><td><input type='submit' name='submit' value='Submit'/></td></tr>
 </table>
 
 </form>
-<?php
-session_start();
-if(isset($_POST['submit']))
-{
 
-$addr = 'csdb.brockport.edu';
-$user = 'wdean2';
-$pass = 'csc423?';
-$db = 'fal16_csc423_wdean2';
-$db = new mysqli("$addr", "$user", "$pass", "$db") or die ("Unable to Connect");
-echo("Connected to Database<br>");
- {
-   $query=mysql_query("Select * From Vendor Where VendorId='".$name."' And Password='".$pwd."'") or die(mysql_error());
-   $res=mysql_fetch_row($query);
-   if($res)
-   {
-    $_SESSION['name']=$name;
-    header('location:index.php');
-   }
-   else
-   {
-    echo'You entered username or password is incorrect';
-   }
- }
-  echo'Enter both username and password';
- }
-
-?>
 
 <?php
 // Footer
