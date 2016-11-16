097cfdeb7d136fb5390d2d5ef8fdb8225c1fb95f
Brian Scarbrough <Brian Scarbrough>
2016-11-16 17:28:17 -0500

    Brian changed index.php to account for selectOrderToAdd.php

--- public_html/index.php
+++ public_html/index.php
@@ -22,7 +22,7 @@ include_once('./templates/header.php');
 	<tr>
 		<td><a href='./selectOrderVendor.php' class='button'>Create An Order</a></td>
 		<td><a href='./viewOrderLogin.php' class='button'>View An Order</a></td>
-		<td><a href='./addItemToExistingOrder.php' class='button'>Add Item to <br> Existing Order</a></td>
+		<td><a href='./selectOrderToAdd.php' class='button'>Add Item to <br> Existing Order</a></td>
 	</tr>	
 	<tr>
 		<td><a href='./addVendor.php' class='button'>Add A Vendor</a></td>
