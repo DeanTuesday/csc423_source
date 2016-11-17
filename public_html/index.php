c669737afdcb4a0ed71f5a088890fe15a8e25406
Corey <ceast4@u.brockport.edu>
2016-11-17 16:17:52 -0500

    marlin8

--- public_html/index.php
+++ public_html/index.php
@@ -38,10 +38,12 @@ include_once('./templates/header.php');
 		<td><a href='./selectCustomerToUpdate.php' class='button'>Update A Customer</a></td>
 	</tr>
 	<tr>
-		//<td><a href='./selectVendorToDelete.php' class='button'>Delete A Vendor</a></td>
-		//<td><a href='./selectItemToDelete.php' class='button'>Delete An Item</a></td>
-		//<td><a href='./selectLocationToDelete.php' class='button'>Delete A Location</a></td>
-		//<td><a href='./selectCustomerToDelete.php' class='button'>Delete A Customer</a></td>
+<!--
+		<td><a href='./selectVendorToDelete.php' class='button'>Delete A Vendor</a></td>
+		<td><a href='./selectItemToDelete.php' class='button'>Delete An Item</a></td>
+		<td><a href='./selectLocationToDelete.php' class='button'>Delete A Location</a></td>
+		<td><a href='./selectCustomerToDelete.php' class='button'>Delete A Customer</a></td>
+-->
 		<td><a href='./selectDeliveryToProcess.php' class='button'>Process a Delivery</a></td>
 	</tr>
 </table>
